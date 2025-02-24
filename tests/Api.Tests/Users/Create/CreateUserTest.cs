15e1895f27cd377f82adb68988fb57c1cd735083
LeoAzevedo59 <leofrancoazevedo@gmail.com>
2025-02-23 22:20:33 -0300

    test: `testIntegration` erro na requisição endpoint `POST api/users`

--- tests/Api.Tests/Users/Create/CreateUserTest.cs
+++ tests/Api.Tests/Users/Create/CreateUserTest.cs
@@ -42,5 +42,50 @@ namespace Api.Tests.Users.Create
             Assert.NotNull(accessToken);
             Assert.NotEmpty(accessToken);
         }
+
+        [Fact]
+        public async Task UserCreate_Fails_EmptyName()
+        {
+            // ACT
+            _request.Name = "test-com-mais-de-32-caracteres-error";
+            _request.Email = "test-gmail.com";
+            _request.Password = string.Empty;
+
+            HttpResponseMessage result =
+                await _httpClient.PostAsJsonAsync(EndPoint, _request);
+
+            // ASSERT
+            string bodyResponse =
+                await result.Content.ReadAsStringAsync();
+
+            JsonDocument response = JsonDocument.Parse(bodyResponse);
+
+            string? name = response.RootElement
+                .GetProperty("name").GetString();
+
+            string? action = response.RootElement
+                .GetProperty("action").GetString();
+
+            int statusCode = response.RootElement
+                .GetProperty("status_code").GetInt16();
+
+            List<string> messages = response.RootElement
+                .GetProperty("message")
+                .EnumerateArray()
+                .Select(element => element.GetString()!)
+                .ToList();
+
+            Assert.False(result.IsSuccessStatusCode);
+            Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+
+            Assert.Equal("ErrorOnValidationException", name);
+            Assert.Equal("Valide os campos obrigatórios.", action);
+            Assert.Equal(HttpStatusCode.BadRequest, (HttpStatusCode)statusCode);
+            Assert.Equal(3, messages.Count);
+            Assert.Contains("Nome deve conter no máximo 32 caracteres.",
+                messages);
+            Assert.Contains("Senha é obrigatório.", messages);
+            Assert.Contains("E-mail não é válido.", messages);
+        }
     }
 }
